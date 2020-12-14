781c9e9c2059d1c95e7ef489de973bbcc3a39c7b
kevinmilet <kevin.milet@gmail.com>
2020-12-14 12:38:17 +0100

    partie 9 ex 5 ok

---
+++ Ex05/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Partie 9 - Ex 5</title>
+</head>
+<body>
+
+    <h1>Exercice 5</h1>
+    <h2>Afficher le nombre de jour qui sépare la date du jour avec le 16 mai 2016.</h2>
+
+    <?php
+        // on stocke la date du 16 mai 2016
+        $startDate = new DateTime('16-05-2016');
+        // on stocke la date du jour
+        $today = new DateTime(date('j-m-y'));
+
+        // on calcule la différence de jour entre aujourd'hui et la 1ere date
+        $nbDays = $today->diff($startDate)->format('%a');
+    ?>
+
+    <p><strong>Nombre de jours entre le 16 mai 2016 et aujourd'hui : </strong><?=$nbDays;?> jours</p>
+    
+
+</body>
+</html>
