832e5f71da9dfe520f07141e679484275bb6c45c
kevinmilet <kevin.milet@gmail.com>
2020-12-14 09:48:56 +0100

    partie 9 ex 2 ok

--- Ex01/index.php
+++ Ex01/index.php
@@ -5,7 +5,10 @@
     <title>Partie 9 - Ex 1</title>
 </head>
 <body>
-    
+
+    <h1>Exercice 1</h1>
+    <h2>Afficher la date courante en respectant la forme jj/mm/aaaa (ex : 16/05/2016).</h2>
+
     <p><strong>Date du jour : </strong><?=date('j/m/Y');?></p>
 
 </body>
---
+++ Ex02/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Partie 9 - Ex 2</title>
+</head>
+<body>
+
+    <h1>Exercice 2</h1>
+    <h2>Afficher la date courante en respectant la forme jj-mm-aa (ex : 16-05-16).</h2>
+    
+    <p><strong>Date du jour : </strong><?=date('j-m-y');?></p>
+
+</body>
+</html>
