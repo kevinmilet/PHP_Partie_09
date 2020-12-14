83c7470d9ce660a69a874a8744fe0994eb9c2420
kevinmilet <kevin.milet@gmail.com>
2020-12-14 09:44:49 +0100

    partie 9 ex 1 ok

---
+++ Ex01/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Partie 9 - Ex 1</title>
+</head>
+<body>
+    
+    <p><strong>Date du jour : </strong><?=date('j/m/Y');?></p>
+
+</body>
+</html>
